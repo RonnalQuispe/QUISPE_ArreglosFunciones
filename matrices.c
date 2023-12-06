aa3e4a3e4fb81ac50e942401f917d90a910e27d3
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-12-06 10:11:41 -0500

    crear la funcion que realice la suma de dos matrices

--- matrices.c
+++ matrices.c
@@ -1,64 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main(int argc, char const *argv[])
-{
+
+void sumatriz(int fila, int columna, int matrizA[fila][columna], int matrizB[fila][columna], int suma[fila][columna]) {//  creamos la funcion sumatriz la cuan incluyen todas las matrices A-B-suma y fila- columna 
+    for (int i = 0; i < fila; i++) {
+        for (int j = 0; j < columna; j++) {
+            suma[i][j] = matrizA[i][j] + matrizB[i][j];// hacemos dos bucle for en la cual se sumen los numeros aleatorios generados en la amtriz A y B, en la cual se proyecte en la amtriz "suma"
+        }
+    }
+}
+
+int main() {
     int fila, columna;
     int i, j;
 
-    printf("ingrese la dimension de la fila:");
+    printf("Ingrese la dimension de la fila: "); //  pedimos al usuario que ingrese la dimension de la matriz en fila y columna 
     scanf("%d", &fila);
-    printf("ingrese la dimension de la columna:");
+    printf("Ingrese la dimension de la columna: ");
     scanf("%d", &columna);
+
     int matrizA[fila][columna];
     int matrizB[fila][columna];
     int suma[fila][columna];
+
     srand(time(0));
-    for (i = 0; i < fila; i++)
-    {
-        for (j = 0; j < columna; j++)
-        {
+    for (i = 0; i < fila; i++) {
+        for (j = 0; j < columna; j++) {
             matrizA[i][j] = rand() % 101; // generamos numeros aleatorios entre 1-100 para la matriz A
             matrizB[i][j] = rand() % 101; // generamos numeros aleatorios entre 1-100 para la matriz B
         }
     }
-    printf("\n");
-    printf("La MATRIZ  1 es:\n");
-    for (i = 0; i < fila; i++)
-    {
-        for (j = 0; j < columna; j++)
-        {
-            printf("%d ", matrizA[i][j]); // imprimimos la Fila y columna de la matriz A
+
+    printf("\nLa MATRIZ 1 es:\n");
+    for (i = 0; i < fila; i++) {
+        for (j = 0; j < columna; j++) {
+            printf("%d ", matrizA[i][j]);
         }
         printf("\n");
-    }
-    printf("\n");
-    printf("la MATRIZ 2 es\n");
-    for (i = 0; i < fila; i++)
-    {
-        for (j = 0; j < columna; j++)
-        {
-            printf("%d ", matrizB[i][j]); // imprimimos la Fila y columna de la matriz A
-        }
-        printf("\n");
-    }
-    for (i = 0; i < fila; i++) // suma matriz
-    {
-        for (j = 0; j < columna; j++)
-        {
-            suma[i][j] = matrizA[i][j] + matrizB[i][j]; // realizamos la suma de la matrizA y matriz B en la matriz "suma"
-        }
     }
 
-    printf("\n");
-    printf("la suma de matriz 1 y matriz2  es:\n");
-    for (i = 0; i < fila; i++)
-    {
-        for (j = 0; j < columna; j++)
-        {
-            printf("%d ", suma[i][j]); // se imprime la matriz "suma"
+    printf("\nLa MATRIZ 2 es:\n");
+    for (i = 0; i < fila; i++) {
+        for (j = 0; j < columna; j++) {
+            printf("%d ", matrizB[i][j]);
         }
-        printf("\n");  //salto de linea
+        printf("\n");
     }
+
+    
     return 0;
 }

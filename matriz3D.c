ebfec5bd463f097299ce1eb7c21648341a264247
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-12-06 10:30:58 -0500

    realizamos la primera  funcion en la cual genera la todas las matrices en 0

--- matriz3D.c
+++ matriz3D.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
+void inicializarMatriz(int x, int y, int z, int tresD[x][y][z]) {
+    // Inicializar todas las matrices en 0
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < z; k++) {
+                tresD[i][j][k] = 0;
+            }
+        }
+    }
+    
+}
+
+
+
+int main() {
     int x, y, z;
-    printf("Ingrese las dimensiones del arreglo tridimensional:\n"); // pedimos al usuario dimesiones de la matriz
-    printf("Primera dimension:");
+    printf("Ingrese las dimensiones del arreglo tridimensional:\n");
+    printf("Primera dimension:");// pdimos al usuario que ingrese la dimension de la matriz 3D
     scanf("%d", &x);
     printf("Segunda dimension:");
     scanf("%d", &y);
@@ -12,38 +25,8 @@ int main()
     scanf("%d", &z);
     int tresD[x][y][z];
 
-    // Inicializar todas las matrices en 0
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            for (int k = 0; k < z; k++)
-            {
-                tresD[i][j][k] = 0;
-            }
-        }
-    }
-    // Inicializar la ultima matriz en unos
-    for (int j = 0; j < y; j++)
-    {
-        for (int k = 0; k < z; k++)
-        {
-            tresD[x - 1][j][k] = 1;
-        }
-    }
-    // Imprimir todos los elementos de la matriz 3d
-    printf("Elementos del arreglo tridimensional:\n");
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            for (int k = 0; k < z; k++)
-            {
-                printf("%d ", tresD[i][j][k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
+    inicializarMatriz(x, y, z, tresD);
+    imprimirMatriz(x, y, z, tresD);
+
     return 0;
 }
